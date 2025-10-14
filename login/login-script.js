fc79efca3408c533f4c141419b6dec98be448d6c
Thao Dang <nguyenthao1012@gmail.com>
2025-10-14 23:50:50 +0700

    update Login page with authen user

--- login/login-script.js
+++ login/login-script.js
@@ -1,3 +1,35 @@
+import { users, currentUser } from '../static/js/mock-data.js';
+
+document.getElementById('show-pw-icon').addEventListener('click', togglePasswordVisible);
+document.getElementById('hide-pw-icon').addEventListener('click', togglePasswordVisible);
+
+
+document.getElementById('login-forms').addEventListener('submit', function (event) {
+    event.preventDefault(); // stops the browser from reloading the page, allowing JavaScript to handle the submission.
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const message = document.getElementById('message');
+    let checked = false;
+    
+    users.some(element => {
+        if ((element.email === email) && (element.password === password)) {
+            checked = true;
+            return true; //stop the 'some' iteration
+        }
+        return false; //continue iteration
+    });
+    console.log(currentUser);
+    if (checked === true) {
+        message.textContent = 'Login successful.';
+        // currentUser = element;
+        window.location.href = '../users/dashboard.html'; //redirect to Dashboard page
+    }
+    else {
+        message.textContent = 'Invalid username or password.';
+    }
+});
+
 function togglePasswordVisible() {
     const pw = document.getElementById("password");
     if (pw.type === "password") {
--- static/js/mock-data.js
+++ static/js/mock-data.js
@@ -1,7 +1,8 @@
 export const users = [
-    {name: "Thao", email: "thao@gmail.com", phone: "0905111111", role: "admin" },
-    {name: "Lien", email: "lien@gmail.com", phone: "0905222222", role: "volunteer" },
-    {name: "Huy", email: "huy@gmail.com", phone: "0905333333", role: "sponsor" },
-    {name: "Thu", email: "thu@gmail.com", phone: "0905444444", role: "teacher" },
-]
+    {id: 1, name: "Nguyen Thao", email: "thao@gmail.com", password: "123456", phone: "0905111111", role: "admin" },
+    {id: 2, name: "Vu Lien", email: "lien@gmail.com", password: "123456", phone: "0905222222", role: "volunteer" },
+    {id: 3, name: "Minh Huy", email: "huy@gmail.com", password: "123456", phone: "0905333333", role: "sponsor" },
+    {id: 4, name: "Ngoc Thu", email: "thu@gmail.com", password: "123456", phone: "0905444444", role: "teacher" },
+];
 
+export let currentUser = 0;
